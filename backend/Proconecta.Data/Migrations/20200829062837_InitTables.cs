45ac7abb96600e4608b2013b2f298be55638285d
Alberto Barrera <albertobarreraes@gmail.com>
2020-08-29 01:33:55 -0500

    Add .gitignore

---
+++ backend/Proconecta.Data/Migrations/20200829062837_InitTables.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proconecta.Data.Migrations
+{
+    public partial class InitTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(maxLength: 150, nullable: true),
+                    Description = table.Column<string>(maxLength: 8000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(maxLength: 200, nullable: true),
+                    Description = table.Column<string>(maxLength: 8000, nullable: true),
+                    Price = table.Column<double>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(maxLength: 200, nullable: true),
+                    Description = table.Column<string>(maxLength: 8000, nullable: true),
+                    Tags = table.Column<string>(maxLength: 255, nullable: true),
+                    Country = table.Column<string>(maxLength: 100, nullable: true),
+                    State = table.Column<string>(maxLength: 255, nullable: true),
+                    City = table.Column<string>(maxLength: 255, nullable: true),
+                    ZipCode = table.Column<string>(maxLength: 10, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Providers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(maxLength: 200, nullable: true),
+                    Description = table.Column<string>(maxLength: 8000, nullable: true),
+                    Country = table.Column<string>(maxLength: 100, nullable: true),
+                    State = table.Column<string>(maxLength: 255, nullable: true),
+                    City = table.Column<string>(maxLength: 255, nullable: true),
+                    Address = table.Column<string>(maxLength: 255, nullable: true),
+                    ZipCode = table.Column<string>(maxLength: 10, nullable: true),
+                    Latitude = table.Column<double>(nullable: true),
+                    Longitude = table.Column<double>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(maxLength: 36, nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Username = table.Column<string>(maxLength: 100, nullable: true),
+                    Salt = table.Column<string>(maxLength: 128, nullable: true),
+                    PwdHashed = table.Column<string>(maxLength: 128, nullable: true),
+                    Type = table.Column<string>(maxLength: 15, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Providers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
